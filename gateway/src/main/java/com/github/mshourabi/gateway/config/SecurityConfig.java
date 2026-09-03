084ff013c6a8657b043e751aa6907fd7f6b2adf3
Majid <majidshoorabi@gmai.com>
2026-09-03 14:04:47 +0330

    feat - add gateway module

---
+++ gateway/src/main/java/com/github/mshourabi/gateway/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.github.mshourabi.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchange -> exchange
+//
+//                        .pathMatchers(
+//                                "/api/auth/**",
+//                                "/actuator/health"
+//                        ).permitAll()
+//
+//                        .pathMatchers("/api/orders/**")
+//                        .authenticated()
+//
+//                        .pathMatchers("/api/admin/**")
+//                        .hasRole("ADMIN")
+//
+//                        .anyExchange()
+//                        .authenticated()
+//                )
+//                .oauth2ResourceServer(
+//                        oauth2 -> oauth2.jwt()
+//                )
+                .build();
+    }
+}
