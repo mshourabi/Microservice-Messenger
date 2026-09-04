7f7f592a58a96c407f024db87e12476b1d9c4981
Majid <majidshoorabi@gmai.com>
2026-09-04 20:09:33 +0330

    feat - add TelegramSenderClint

---
+++ client/src/main/java/com/github/mshourabi/client/telegramsender/fiegn/TelegramSenderClient.java
@@ -0,0 +1,18 @@
+package com.github.mshourabi.client.telegramsender.fiegn;
+
+import com.github.mshourabi.client.telegramsender.dto.SyncMessageDTO;
+import com.github.mshourabi.client.telegramsender.constants.ApiConstants;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        name = "${telegram-sender.service.name}",
+        url = "${telegram-sender.service.url}"
+)
+public interface TelegramSenderClient {
+
+    @PostMapping(ApiConstants.SEND_SYNC)
+    ResponseEntity<SyncMessageDTO.SendDirectResponse> sendMessage(@RequestBody SyncMessageDTO.SendDirectRequest sendDirectRequest);
+}
