65a76ea97f597692b3ae9733d4870349a2b8465d
Majid <majidshoorabi@gmai.com>
2026-09-05 22:06:45 +0330

    feat - add RequestRateLimiter on GateWay

--- gateway/src/main/java/com/github/mshourabi/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/github/mshourabi/gateway/GatewayApplication.java
@@ -2,12 +2,32 @@ package com.github.mshourabi.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+    /**
+     * This ipKeyResolver bean used in RequestRateLimiter
+     *
+     * @return
+     */
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange ->
+                Mono.just(
+                        exchange.getRequest()
+                                .getRemoteAddress()
+                                .getAddress()
+                                .getHostAddress()
+                );
     }
 
 }
